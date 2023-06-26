bad934d15b2c506b247a6dca8dfc423c3dab0943
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-06-27 02:30:18 +0700

    feat: Create TicketList

---
+++ src/components/TicketList/index.tsx
@@ -0,0 +1,30 @@
+import TicketCard from '../TicketCard/';
+import styles from './styles.module.scss';
+import Movie from '@/entities/movie';
+
+export default function TicketList({
+  movies,
+  enableDelete,
+}: {
+  movies: Movie[];
+  enableDelete: boolean;
+}) {
+  return (
+    <ul className={styles['tickets-list']}>
+      {movies.map((movie) => {
+        const { title, genre, id, posterUrl } = movie;
+        return (
+          <li key={id} className={styles['tickets-list__ticket']}>
+            <TicketCard
+              title={title}
+              genre={genre}
+              id={id}
+              posterUrl={posterUrl}
+              showDelete={enableDelete}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
