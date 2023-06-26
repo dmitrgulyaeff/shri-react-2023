5e23999179951df4c6cabc7de246175677eea87d
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-06-26 15:54:57 +0700

    feat: Add Links to Footer and Header

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,11 @@
 import styles from './styles.module.scss';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <h2 className={styles.footer__text}>Вопросы-ответы</h2>
-      <h2 className={styles.footer__text}>О нас</h2>
+      <Link href='/faq'><h2 className={styles.footer__text}>Вопросы-ответы</h2></Link>
+      <Link href='/about-us'><h2 className={styles.footer__text}>О нас</h2></Link>
     </footer>
   );
 }
