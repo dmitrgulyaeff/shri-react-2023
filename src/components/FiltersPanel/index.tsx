6f77313bfb4307c418f6a54aaa5c0a77e557d294
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-06-27 03:04:49 +0700

    feat: Add props to FiltersPanel

--- src/components/FiltersPanel/index.tsx
+++ src/components/FiltersPanel/index.tsx
@@ -1,19 +1,39 @@
-"use client"
+'use client';
 import styles from './styles.module.scss';
 import Dropdown from '../Dropdown';
 import Input from '../Input';
 
-export default function FiltersPanel() {
+export default function FiltersPanel({
+  inputOnChange,
+  genreOnChange,
+  genreOptions,
+  cinemaOnChange,
+  cinemaOptions,
+}: {
+  inputOnChange: (value: string) => void;
+  genreOnChange: (value: string) => void;
+  cinemaOnChange: (value: string) => void;
+  genreOptions: string[];
+  cinemaOptions: string[];
+
+}) {
   return (
-    
-    <div className={styles['filters-panel']}
-    >
+    <div className={styles['filters-panel']}>
       <h2 className={styles['filters-panel__head']}>Фильтр поиска</h2>
-      <Input placeholder='input' onChange={(str) => {
-        console.log(str)
-      }} label='Название' />
-      <Dropdown label='Жанр' defaultText='Выберите жанр' options={["Фэнтези", "Боевик", "Драма"]} onChange={console.log}/>
-      <Dropdown label='Кинотеатр' defaultText='Выберите кинотеатр' options={['Альянс', "Спутник", "Луч"]} onChange={console.log}/>
+      <Input placeholder="input" onChange={inputOnChange} label="Название" />
+      <Dropdown
+        label="Жанр"
+        defaultText="Выберите жанр"
+        options={genreOptions}
+        onChange={genreOnChange}
+      />
+      <Dropdown
+        label="Кинотеатр"
+        defaultText="Выберите кинотеатр"
+        options={cinemaOptions}
+        onChange={cinemaOnChange}
+      />
     </div>
   );
 }
+2;
