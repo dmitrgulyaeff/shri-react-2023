41bad8111da189c3cec9f3b13466027876e63e8f
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-06-27 02:21:32 +0700

    feat: Add Movie entity

---
+++ src/entities/movie.ts
@@ -0,0 +1,11 @@
+export default interface Movie {
+  title: string;
+  posterUrl: string;
+  releaseYear: number;
+  description: string;
+  genre: string;
+  id: string;
+  rating: number;
+  director: string;
+  reviewIds: string[];
+}
