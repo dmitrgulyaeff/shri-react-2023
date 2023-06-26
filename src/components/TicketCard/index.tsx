f64eee0fb936fa8abd8737049bc33d5cb835dafb
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-06-27 02:33:36 +0700

    refactor: Add props and use TicketCounter

--- src/components/TicketCard/index.tsx
+++ src/components/TicketCard/index.tsx
@@ -2,54 +2,32 @@ import Image from 'next/image';
 import styles from './styles.module.scss';
 import filmCover from '$/images/The_Lord_of_the_Rings._The_Fellowship_of_the_Ring_—_movie.jpg';
 
-export default function TicketCard() {
+import TicketCounter from '../TicketCounter';
+
+interface TicketCardProps {
+  title: string;
+  genre: string;
+  posterUrl?: string;
+  id: string;
+  showDelete: boolean;
+}
+
+export default function TicketCard({title, genre, id, posterUrl, showDelete}:TicketCardProps) {
   return (
     <article className={styles['ticket']}>
       <Image
         className={styles['ticket__poster']}
-        src={filmCover}
+        src={posterUrl ? posterUrl : filmCover}
         width={100}
         height={120}
         alt="film cover"
       />
       <div className={styles['ticket__info']}>
-        <h2 className={styles['ticket__info-name']}>FilmName</h2>
-        <span className={styles['ticket__info-genre']}>genre</span>
+        <h2 className={styles['ticket__info-name']}>{title}</h2>
+        <span className={styles['ticket__info-genre']}>{genre}</span>
       </div>
-      <div className={styles['ticket__counter']}>
-        <button className={styles['ticket__counter-minus']} type="button" title="Minus">
-          <svg
-            width="20"
-            height="20"
-            viewBox="0 0 20 20"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect width="20" height="20" rx="4" fill="#FF5500" />
-            <path
-              d="M14.5 10C14.5 10.0995 14.4605 10.1948 14.3902 10.2652C14.3198 10.3355 14.2245 10.375 14.125 10.375H5.875C5.77554 10.375 5.68016 10.3355 5.60984 10.2652C5.53951 10.1948 5.5 10.0995 5.5 10C5.5 9.90054 5.53951 9.80516 5.60984 9.73484C5.68016 9.66451 5.77554 9.625 5.875 9.625H14.125C14.2245 9.625 14.3198 9.66451 14.3902 9.73484C14.4605 9.80516 14.5 9.90054 14.5 10Z"
-              fill="white"
-            />
-          </svg>
-        </button>
-        <span className={styles['ticket__counter-count']}>7</span>
-        <button className={styles['ticket__counter-plus']} type="button" title="Plus">
-          <svg
-            width="20"
-            height="20"
-            viewBox="0 0 20 20"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect width="20" height="20" rx="4" fill="#FF5500" />
-            <path
-              d="M14.5 10C14.5 10.0995 14.4605 10.1948 14.3902 10.2652C14.3198 10.3355 14.2245 10.375 14.125 10.375H10.375V14.125C10.375 14.2245 10.3355 14.3198 10.2652 14.3902C10.1948 14.4605 10.0995 14.5 10 14.5C9.90054 14.5 9.80516 14.4605 9.73484 14.3902C9.66451 14.3198 9.625 14.2245 9.625 14.125V10.375H5.875C5.77554 10.375 5.68016 10.3355 5.60984 10.2652C5.53951 10.1948 5.5 10.0995 5.5 10C5.5 9.90054 5.53951 9.80516 5.60984 9.73484C5.68016 9.66451 5.77554 9.625 5.875 9.625H9.625V5.875C9.625 5.77554 9.66451 5.68016 9.73484 5.60984C9.80516 5.53951 9.90054 5.5 10 5.5C10.0995 5.5 10.1948 5.53951 10.2652 5.60984C10.3355 5.68016 10.375 5.77554 10.375 5.875V9.625H14.125C14.2245 9.625 14.3198 9.66451 14.3902 9.73484C14.4605 9.80516 14.5 9.90054 14.5 10Z"
-              fill="white"
-            />
-          </svg>
-        </button>
-      </div>
-      <button className={styles['ticket__delete']} type="button" title="Delete">
+      <TicketCounter id={id}/>
+     { showDelete && <button className={styles['ticket__delete']} type="button" title="Delete">
         <svg
           width="20"
           height="20"
@@ -62,7 +40,7 @@ export default function TicketCard() {
             fill="#333333"
           />
         </svg>
-      </button>
+      </button>}
     </article>
   );
 }
