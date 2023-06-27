bd9335acc35162052d6396649e25024b97b6e335
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-06-27 07:11:31 +0700

    feat: Enable basket page

--- src/app/basket/page.tsx
+++ src/app/basket/page.tsx
@@ -1,18 +1,26 @@
-import TicketCard from "@/components/TicketCard"
-import styles from './styles.module.scss'
+"use client";
 
-export default function Basket () {
+import TicketList from '@/components/TicketList';
+import styles from './styles.module.scss';
+import { useSelector } from 'react-redux';
+import Movie from '@/entities/movie';
+import { selectCartModule } from '@/redux/features/cart/selector';
+import { useGetMoviesQuery } from '@/redux/services/movieApi';
+
+export default function Basket() {
+  const cart = useSelector((state) => selectCartModule(state));
+  const { data: movies, isLoading, error } = useGetMoviesQuery(null);
+
+  if (isLoading) {return (<div>Loading...</div>)};
+  if (!movies || error) {return <div>404</div>}
+  const tickets = movies.filter((movie: Movie) => {
+    return movie.id in cart;
+  });
+
   return (
-    <div style={{padding: 24, display: 'flex', flexDirection: 'column', gap: 10 }}>
-      <TicketCard />
-      <TicketCard />
-      <TicketCard />
-      <TicketCard />
-      <TicketCard />
-      <TicketCard />
-      <TicketCard />
-      <TicketCard />
-      <TicketCard />
+    <div className={styles['ticket-basket']}>
+      {tickets.length !== 0 ? <TicketList enableDelete={true} movies={tickets} /> : <div>
+        Корзина пуста</div>}
     </div>
-  )
+  );
 }
