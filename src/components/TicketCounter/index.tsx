a7a0f7c41701d20c37b40deda236932ad6c64c07
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-06-27 16:31:04 +0700

    fix: Add styles for counter buttons

--- src/components/TicketCounter/index.tsx
+++ src/components/TicketCounter/index.tsx
@@ -3,8 +3,11 @@
 import { cartActions } from '@/redux/features/cart';
 import { selectProductAmount } from '@/redux/features/cart/selector';
 import { useDispatch, useSelector } from 'react-redux';
+import { selectTotalTickets } from '@/redux/features/cart/selector';
 import Minus from '$/images/icons/counterMinus.svg';
+import MinusDisabled from '$/images/icons/counterMinusDisabled.svg';
 import Plus from '$/images/icons/counterPlus.svg';
+import PlusDisabled from '$/images/icons/counterPlusDisabled.svg';
 import styles from './styles.module.scss';
 
 interface TicketCounterProps {
@@ -13,11 +16,8 @@ interface TicketCounterProps {
 
 export default function TicketCounter({ id }: TicketCounterProps) {
   const dispatch = useDispatch();
-
-  const Product = () => {
-    const amount = useSelector((state) => selectProductAmount(state, id));
-    return <span className={styles['ticket__counter-count']}>{amount}</span>;
-  };
+  const totalTickets = useSelector((state) => selectTotalTickets(state));
+  const amount = useSelector((state) => selectProductAmount(state, id));
 
   return (
     <div className={styles['ticket__counter']}>
@@ -27,16 +27,19 @@ export default function TicketCounter({ id }: TicketCounterProps) {
         title="Minus"
         onClick={() => dispatch(cartActions.decrement(id))}
       >
-        <Minus />
+  
+      {amount === 0 ? <MinusDisabled /> : <Minus /> }
       </button>
-      <Product />
+      <span className={styles['ticket__counter-count']}>{amount}</span>
       <button
         className={styles['ticket__counter-plus']}
         type="button"
         title="Plus"
         onClick={() => dispatch(cartActions.increment(id))}
       >
-        <Plus />
+      {totalTickets === 30 ? <PlusDisabled /> : <Plus /> }
+
+        
       </button>
     </div>
   );
