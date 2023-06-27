7f9b3a18381eb83f1d7484ba190654aa3f81038f
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-06-27 13:37:50 +0700

    feat: Add total tickets to Basket

--- src/app/basket/page.tsx
+++ src/app/basket/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import TicketList from '@/components/TicketList';
 import styles from './styles.module.scss';
@@ -6,21 +6,37 @@ import { useSelector } from 'react-redux';
 import Movie from '@/entities/movie';
 import { selectCartModule } from '@/redux/features/cart/selector';
 import { useGetMoviesQuery } from '@/redux/services/movieApi';
+import { selectTotalTickets } from '@/redux/features/cart/selector';
 
 export default function Basket() {
+  const totalTickets = useSelector((state) => selectTotalTickets(state));
+
   const cart = useSelector((state) => selectCartModule(state));
   const { data: movies, isLoading, error } = useGetMoviesQuery(null);
 
-  if (isLoading) {return (<div>Loading...</div>)};
-  if (!movies || error) {return <div>404</div>}
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (!movies || error) {
+    return <div>404</div>;
+  }
   const tickets = movies.filter((movie: Movie) => {
     return movie.id in cart;
   });
 
   return (
     <div className={styles['ticket-basket']}>
-      {tickets.length !== 0 ? <TicketList enableDelete={true} movies={tickets} /> : <div>
-        Корзина пуста</div>}
+      {tickets.length !== 0 ? (
+        <>
+          <TicketList enableDelete={true} movies={tickets}/>
+          <div className={styles['ticket-basket__total']}>
+            <h2 className={styles['ticket-basket__total-head']}>Итого билетов:</h2>
+            <span className={styles['ticket-basket__total-value']}>{totalTickets}</span>
+          </div>
+        </>
+      ) : (
+        <div>Корзина пуста</div>
+      )}
     </div>
   );
 }
